9e4ddb4e06e1e11fb180d416ab7f1307d66e8f56
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2024-12-20 23:54:04 +0600

    169

---
+++ LeetCode.Solutions/LeetCode_00169.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Solutions;
+
+internal class LeetCode_00169
+{
+    // problem name: Majority Element
+    // problem url: https://leetcode.com/problems/majority-element/description/
+
+    // Author: Mehedi Hasan Kajol
+    // Date: 20 Dec 2024
+    // Profile: https://leetcode.com/u/Mehedi/
+
+    public int MajorityElement(int[] nums)
+    {
+        var valuesTable = new Dictionary<int, int>();
+        foreach (var item in nums)
+        {
+            if (valuesTable.ContainsKey(item))
+            {
+                valuesTable[item]++;
+            }
+            else
+            {
+                valuesTable.Add(item, 1);
+            }
+
+            if (valuesTable[item] > nums.Length / 2)
+                return item;
+        }
+        return 0;
+    }
+}
---
+++ LeetCode.Solutions/Program.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Solutions;
+
+var data = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+
+var obj = new LeetCode_00169();
+
+var result = obj.MajorityElement(data);
+
+PrintInt(result);
+
+#region Print Methods
+
+void PrintInt(int data)
+{
+    Console.WriteLine(data);
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+#endregion
