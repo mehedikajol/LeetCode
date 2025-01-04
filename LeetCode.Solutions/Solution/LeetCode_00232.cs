df179d774276148fec1928adaac0a26475fb5b20
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2025-01-04 10:11:06 +0600

    232

---
+++ LeetCode.Solutions/Solution/LeetCode_00232.cs
@@ -0,0 +1,65 @@
+﻿// problem name: Implement Queue using Stacks
+// problem url: https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+// Author: Mehedi Hasan Kajol
+// Date: 04 Jan 2025
+// Profile: https://leetcode.com/u/Mehedi/ 
+
+namespace LeetCode.Solutions.Solution;
+
+internal class LeetCode_00232
+{
+    internal class MyQueue
+    {
+        Stack<int> queue;
+        Stack<int> stack;
+
+        public MyQueue()
+        {
+            queue = [];
+            stack = [];
+        }
+
+        public void Push(int x)
+        {
+            queue.Push(x);
+        }
+
+        public int Pop()
+        {
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Pop());
+            }
+            var data = stack.Pop();
+
+            while (stack.Count > 0)
+            {
+                queue.Push(stack.Pop());
+            }
+
+            return data;
+        }
+
+        public int Peek()
+        {
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Pop());
+            }
+            var data = stack.Peek();
+
+            while (stack.Count > 0)
+            {
+                queue.Push(stack.Pop());
+            }
+
+            return data;
+        }
+
+        public bool Empty()
+        {
+            return !queue.Any();
+        }
+    }
+}
